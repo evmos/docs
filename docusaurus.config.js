b7ad3a38bd703e9b3989c41959ebd48184605af4
Johnny Hung <hungmasterj@gmail.com>
2023-02-24 15:15:17 -0700

    algolia and updated link issue (#53)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -226,6 +226,34 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'DPTADG0ME1',
+  
+        // Public API key: it is safe to commit it
+        apiKey: 'fa49947284cf431e40b9d65a4d88c578',
+  
+        indexName: 'evmosdocs',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        placeholder: 'Search Evmos Docs',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
     }),
 };
 
