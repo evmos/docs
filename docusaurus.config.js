e014b6fe48060838bd14f4b41c5f3ad6229020c3
Johnny Hung <hungmasterj@gmail.com>
2023-03-26 23:00:25 -0700

    added tailwind and add testnet to keplr

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,6 +114,17 @@ const config = {
         disableInDev: false,
       },
     ],
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
   ],
 
   themeConfig:
---
+++ src/utils/ChainConfig.js
@@ -0,0 +1,71 @@
+import Swal from 'sweetalert2'
+
+export const chainConfig = {
+  stakeCurrency: {
+    coinDenom: "TEVMOS",
+    coinMinimalDenom: "atevmos",
+    coinDecimals: 18,
+  },
+  bech32Config: {
+    bech32PrefixAccAddr: "evmos",
+    bech32PrefixAccPub: "evmospub",
+    bech32PrefixValAddr: "evmosvaloper",
+    bech32PrefixValPub: "evmosvaloperpub",
+    bech32PrefixConsAddr: "evmosvalcons",
+    bech32PrefixConsPub: "evmosvalconspub",
+  },
+  currencies: [
+    {
+      coinDenom: "TEVMOS",
+      coinMinimalDenom: "atevmos",
+      coinDecimals: 18,
+    },
+  ],
+  feeCurrencies: [
+    {
+      coinDenom: "TEVMOS",
+      coinMinimalDenom: "atevmos",
+      coinDecimals: 18,
+      gasPriceStep: {
+        low: 25000000000,
+        average: 25000000000,
+        high: 40000000000,
+      },
+    },
+  ],
+  bip44: {
+    coinType: 60,
+  },
+  chainId: "evmos_9000-4",
+  chainName: "Evmos Testnet",
+  rpc: "https://tendermint.bd.evmos.dev:26657",
+  rest: "https://rest.bd.evmos.dev:1317",
+  beta: true,
+  features: ["ibc-transfer", "ibc-go", "eth-address-gen", "eth-key-sign"],
+};
+
+export const handler = async () => {
+  if (window.keplr) {
+    try {
+      await window.keplr.enable(chainConfig.chainId);
+      Swal.fire({
+        title: 'Note',
+        text: `Testnet ${chainConfig.chainId} has already added`,
+        icon: 'info',
+        confirmButtonText: 'Awesome'
+      });
+    } catch (e) {
+      try {
+        await window.keplr.experimentalSuggestChain(chainConfig);
+        await window.keplr.enable(chainConfig.chainId);
+      } catch (e2) {
+        Swal.fire({
+            title: 'Error',
+            text: e2?.message,
+            icon: 'error',
+            confirmButtonText: 'Ok'
+          });
+      }
+    }
+  }
+};
